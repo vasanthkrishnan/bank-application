a9dfa0c542e3e5c77150ccba355fa5277d8814f6
vasanthkrishnan <vasanthakrishnans22it@srishakthi.ac.in>
2024-11-25 06:39:38 +0530

    first-commit

---
+++ src/bank/main/Main.java
@@ -0,0 +1,17 @@
+package bank.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        System.out.println("Enter your username: ");
+        String username = input.nextLine();
+        System.out.println("Enter your password");
+        String password = input.nextLine();
+
+    }
+}
